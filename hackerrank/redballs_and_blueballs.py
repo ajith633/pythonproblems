c1ac0fa67f1861607adeeca0b8a6a9643cee0f25
AJITH THOUTU <ajith633@yahoo.com>
2020-10-03 13:38:30 -0700

    Adding hackerrank solutions

--- hackerrank/redballs_and_blueballs.py
+++ hackerrank/redballs_and_blueballs.py
@@ -1,13 +1,12 @@
 import sys
 import itertools
 from fractions import Fraction
-#https://www.hackerrank.com/challenges/s10-mcq-3/problem
 
-""" this is my program"""
 if __name__ == '__main__':
-    urn1=['R', 'R', 'R', 'R', 'B', 'B', 'B']
-    urn2=['R', 'R', 'R', 'R', 'R', 'B', 'B', 'B',  'B']
-    urn3=['R', 'R', 'R', 'R', 'B', 'B', 'B', 'B']
+    urn1=4*['R']+3*['B']
+    urn2=5*['R']+4*['B']
+    urn3=4*['R']+4*['B']
+
 
     p1=Fraction(urn1.count('R'),len(urn1))
     p2=Fraction(urn2.count('R'),len(urn2))
@@ -16,9 +15,7 @@ if __name__ == '__main__':
     prob=(p1*p2*(1-p3)+p1*(1-p2)*p3+(1-p1)*p2*p3)
     print(prob)
 
-
-
-
+    #Inefficient implmentation by using product
     tf=[x.count('R')==2 for x in (list(itertools.product(urn1,urn2,urn3)))]
     print(Fraction(tf.count(True),len(tf)))
 
